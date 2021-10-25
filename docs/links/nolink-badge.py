8ca930b486d7eb634d71c7ab4019f33b2f399a01
Jessica Scheick <jbscheick@gmail.com>
2021-10-25 16:56:56 -0400

    try more syntax changes

--- docs/links/nolink-badge.py
+++ docs/links/nolink-badge.py
@@ -1,7 +1,11 @@
 import link
 
-link_name = "Sphinx External Links"
-user_text = "External Links Extension"
-url = "http://www.sphinx-doc.org/en/stable/ext/extlinks.html"
+link_name = "No link badge"
+user_text = "get from input"
+color = "orange"
+badge_img_url = f"https://img.shields.io/static/v1?label=&message=web&nbsp;development&color={color}"
+dummy_url = "https://"
 
-link.xref_links.update({link_name: (user_text, url)})
+badge_url = "![label](" + badge_img_url + ")]"
+
+link.xref_links.update({link_name: (badge_url, dummy_url)})
