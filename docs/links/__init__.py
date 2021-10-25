3e8ce4faaeece3bf08cef9ecd06ae08a3edc2add
Jessica Scheick <jbscheick@gmail.com>
2021-10-25 16:23:41 -0400

    undo changes to links __init__.py

--- docs/links/__init__.py
+++ docs/links/__init__.py
@@ -1,11 +1,9 @@
-# from os.path import dirname, basename, isfile
+from os.path import dirname, basename, isfile
 
-# import glob
-# modules = glob.glob(dirname(__file__)+"/*.py")
+import glob
+modules = glob.glob(dirname(__file__)+"/*.py")
 
-# __all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
-
-import sphinx.ext.extlinks as links
+__all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
 
 from links.link import *
 from links import *
