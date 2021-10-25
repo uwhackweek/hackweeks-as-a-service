199bb30ee5e8bd7afbfb281a8ed99dc72a9bb665
Jessica Scheick <jbscheick@gmail.com>
2021-10-25 16:35:29 -0400

    setup external links

--- docs/links/__init__.py
+++ docs/links/__init__.py
@@ -5,5 +5,5 @@ modules = glob.glob(dirname(__file__)+"/*.py")
 
 __all__ = [ basename(f)[:-3] for f in modules if isfile(f)]
 
-from links.link import *
-from links import *
+# from links.link import *
+# from links import *
